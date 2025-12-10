74739863dde77c5cb2bf64f00608d7e6c71e11b2
MxrsExe <ximmarshd@gmail.com>
2025-12-10 22:20:38 +0100

    Adds app routes and corresponding htmls

--- app.py
+++ app.py
@@ -12,14 +12,40 @@ def index():
         return "This is a POST request"
     return "Hello, World! Get Request Received"
     
+@app.route("/login/")
+def login():
+    return render_template("login.html")
 
-@app.route("/lists/")
-def lists():
-    return render_template("list.html", list = list) 
+@app.route("/welcome/")
+def create_or_join_wg():
+    return render_template("welcome.html")
 
-@app.route("/lists/<int:id>")
-def list(id):
-    return f"Todo: This is the lists page for a list with id {id}."
+@app.route("/welcome/create_wg/")
+def create_wg():
+    return render_template("create_wg.html")
 
+@app.route("/welcome/join_wg/")
+def join_wg():
+    return render_template("join_wg.html")
+
+@app.route("/dashboard/")
+def dashboard():
+    return render_template("dashboard.html")
+
+@app.route("/putzplan/")
+def putzplan():
+    return render_template("putzplan.html")
+
+@app.route("/innovationboard/")
+def innovationboard():
+    return render_template("innovationboard.html")
+
+@app.route("/activtyboard/")
+def activityboard():
+    return render_template("activityboard.html")
+
+@app.route("/einkaufsliste/")
+def einkaufsliste():
+    return render_template("einkaufsliste.html")
 
 
