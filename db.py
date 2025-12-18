07367121635bd8eb586cff8cf432dc57661e3708
Jane Doe <s_pietzuch24@stud.hwr-berlin.de>
2025-12-18 12:22:49 +0100

    first database commit

--- db.py
+++ db.py
@@ -1,24 +1,34 @@
-import sqlite3
+# import sqlite3
 
-from flask import current_app, g
+# from flask import current_app, g
+# from flask_sqlalchemy import SQLAlchemy
+
+# db = SQLAlchemy()
+import click
+
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import orm
+from app import app
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todos.sqlite'
 
 db = SQLAlchemy()
+db.init_app(app)
 
+class Shopping_Category(db.Model):
+    
+# def db_connection():
+#     if 'db_con' not in g:
+#         g.db_con = sqlite3.connect(current_app.config['DATABASE'])
+#     return db_con
 
+# def db_close(db_con):
+#     db_con = g.pop('db_con', None)
+#     if db_con is not None:
+#         db_con.close()
 
-def db_connection():
-    if 'db_con' not in g:
-        g.db_con = sqlite3.connect(current_app.config['DATABASE'])
-    return db_con
+# sql_query = "SELECT * FROM list ORDER BY name;"
 
-def db_close(db_con):
-    db_con = g.pop('db_con', None)
-    if db_con is not None:
-        db_con.close()
-
-sql_query = "SELECT * FROM list ORDER BY name;"
-
-db_con = db_connection()
-lists = db_con.execute(sql_query)
-db_close(db_con)
+# db_con = db_connection()
+# lists = db_con.execute(sql_query)
+# db_close(db_con)
