ceb94fa014f122fd9c85888f2b7bfa2173ffc647
Jane Doe <s_pietzuch24@stud.hwr-berlin.de>
2026-01-08 17:33:30 +0100

    forms.py erstellt

---
+++ forms.py
@@ -0,0 +1,29 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtform.validators import DataRequired, Email, Length
+
+class LoginForm(FlaskForm):
+    username = StringField(
+        "Benutzername",
+        validators=[DataRequired(), Length(min=3, max=50)]
+    )
+    password = PasswordField(
+        "Passwort",
+        validators=[DataRequired()]
+    )
+    submit = SubmitField("Login")
+
+ class RegisterForm(FlaskForm):
+    username = StringField(
+        "Benutzername",
+        validators=[DataRequired(), Length(min=3, max=50)]
+    )
+    email = StringField(
+        "E-Mail",
+        validators=[DataRequired(), Email()]
+    )
+    password = PasswordField(
+        "Passwort",
+        validators=[DataRequired(), Length(min=6)]
+    )
+    submit = SubmitField("Registrieren")
