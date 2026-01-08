2f5639928ead3e1d301828c247854f5727ce51e5
Jane Doe <s_pietzuch24@stud.hwr-berlin.de>
2026-01-08 19:01:49 +0100

    EqualTo bei forms.py hinzugefügt

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Email, Length
+from wtforms.validators import DataRequired, Email, Length, EqualTo
 
 class LoginForm(FlaskForm):
     username = StringField(
