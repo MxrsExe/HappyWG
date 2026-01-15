0c6ca736f114cad70cc2604b21a283b6566a834b
Jane Doe <s_pietzuch24@stud.hwr-berlin.de>
2026-01-15 13:24:38 +0100

    created_at bei ShoppingItem zur DB migrated

--- db.py
+++ db.py
@@ -7,7 +7,7 @@
 
 
 
-
+from datetime import datetime
 import click
 
 # We import SQLAlchemy from the Flask-SQLAlchemy package, and also the object app from our own app module (i.e., from file 📄app.py).
@@ -67,7 +67,7 @@ class ShoppingItem(db.Model):
     assigned_to = db.Column(db.Integer, db.ForeignKey('USER.user_id'), nullable=True)
     name = db.Column(db.String, nullable=False)
     quantity = db.Column(db.String)
-    created_at = db.Colum(db.DateTime, default=datetime.now)
+    created_at = db.Column(db.DateTime, default=datetime.now)
     #Beziehungen
     wg = db.relationship('Wg', back_populates='shopping_items')
     added_by_user = db.relationship('User', foreign_keys=[added_by], back_populates='added_items')
